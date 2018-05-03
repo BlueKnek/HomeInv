09bca5566bdc31e38f61899a1b7e02756e2e0a22
BlueKnek <blueknek@gmail.com>
2018-05-03 13:45:18 +0200

    Init item model

--- django-homeinv/app_homeinv/models.py
+++ django-homeinv/app_homeinv/models.py
@@ -1,3 +1,12 @@
+import json
+
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    photo = models.ImageField(upload_to='items')
+
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
