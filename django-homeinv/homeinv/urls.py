efb4a75ab8453023f881b9612d33826231a9ff65
BlueKnek <blueknek@gmail.com>
2018-05-03 13:52:15 +0200

    Name index

--- django-homeinv/homeinv/urls.py
+++ django-homeinv/homeinv/urls.py
@@ -19,9 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
-    path('admin/', admin.site.urls),
-]
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('admin/', admin.site.urls),
 ]
 
