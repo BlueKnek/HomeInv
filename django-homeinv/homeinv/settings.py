f0f92baa42a4b5d30d43b11bf96136e41c1d1a20
BlueKnek <blueknek@gmail.com>
2018-05-03 09:47:01 +0200

    Use var dir for changing files

--- django-homeinv/homeinv/settings.py
+++ django-homeinv/homeinv/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+VAR_DIR = os.path.join(BASE_DIR, 'var')
 
 
 # Quick-start development settings - unsuitable for production
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'homeinv.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(VAR_DIR, 'db.sqlite3'),
     }
 }
 
