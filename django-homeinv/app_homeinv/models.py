ff613dfcf8e9f90d4ec83e121c368e0305724065
BlueKnek <blueknek@gmail.com>
2018-05-04 09:16:08 +0200

    Add group model

--- django-homeinv/app_homeinv/models.py
+++ django-homeinv/app_homeinv/models.py
@@ -4,10 +4,23 @@ from django.db import models
 from django.urls import reverse_lazy
 
 
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    photo = models.ImageField(upload_to='groups')
+
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def get_absolute_url(self):
+        return reverse_lazy('homeinv:group-detail', kwargs={'pk': self.pk})
+
+
 class Item(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     photo = models.ImageField(upload_to='items')
+    groups = models.ManyToManyField(Group)
 
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
