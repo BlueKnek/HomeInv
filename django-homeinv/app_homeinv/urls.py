47eb954216dfa7960003abc7a248ecfec4609d78
BlueKnek <blueknek@gmail.com>
2018-05-04 09:01:01 +0200

    Fix linter

--- django-homeinv/app_homeinv/urls.py
+++ django-homeinv/app_homeinv/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     path('item/create', views.ItemCreateView.as_view(), name='item-create'),
     path('item/<pk>', views.ItemDetailView.as_view(), name='item-detail'),
     path('item/<pk>/update', views.ItemUpdateView.as_view(), name='item-update'),
-    path('item/<pk>/delete', views.ItemDeleteView.as_view(), name='item-delete')
+    path('item/<pk>/delete', views.ItemDeleteView.as_view(), name='item-delete'),
 ]
-
