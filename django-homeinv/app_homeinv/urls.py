b7b57b4fe3fad96f50555c37a354ce05864702c0
BlueKnek <blueknek@gmail.com>
2018-05-04 09:49:19 +0200

    Init group views

--- django-homeinv/app_homeinv/urls.py
+++ django-homeinv/app_homeinv/urls.py
@@ -5,6 +5,14 @@ from . import views
 app_name = 'app_homeinv'
 
 urlpatterns = [
+    # Group
+    path('groups', views.GroupListView.as_view(), name='group-list'),
+    path('group/create', views.GroupCreateView.as_view(), name='group-create'),
+    path('group/<pk>', views.GroupDetailView.as_view(), name='group-detail'),
+    path('group/<pk>/update', views.GroupUpdateView.as_view(), name='group-update'),
+    path('group/<pk>/delete', views.GroupDeleteView.as_view(), name='group-delete'),
+
+    # Item
     path('items', views.ItemListView.as_view(), name='item-list'),
     path('item/create', views.ItemCreateView.as_view(), name='item-create'),
     path('item/<pk>', views.ItemDetailView.as_view(), name='item-detail'),
--- django-homeinv/app_homeinv/views.py
+++ django-homeinv/app_homeinv/views.py
@@ -5,9 +5,36 @@ from django.urls import reverse_lazy
 from . import models
 
 
+# Group views
+
+class GroupCreateView(generic.CreateView):
+    model = models.Group
+    fields = ['name', 'description', 'photo']
+
+
+class GroupDetailView(generic.DetailView):
+    model = models.Group
+
+
+class GroupUpdateView(generic.UpdateView):
+    model = models.Group
+    fields = ['name', 'description', 'photo']
+
+
+class GroupDeleteView(generic.DeleteView):
+    model = models.Group
+    success_url = reverse_lazy('index')
+
+
+class GroupListView(generic.ListView):
+    model = models.Group
+
+
+# Item views
+
 class ItemCreateView(generic.CreateView):
     model = models.Item
-    fields = ['name', 'description', 'photo']
+    fields = ['name', 'description', 'photo', 'groups']
 
 
 class ItemDetailView(generic.DetailView):
